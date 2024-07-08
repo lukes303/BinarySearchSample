127ad644bba2f1db6ea49147ddd63fca259bb3f2
Luke Selby <lukeselby3@gmail.com>
2024-07-08 17:44:24 -0500

    small fixes

--- main.cpp
+++ main.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int BSearch(int arr[], int key, int left, int right) {
 
+	if(left > right) return -1;
+
 	int mid = (left + right) / 2;
 
 	int val = arr[mid];
-
-	cout << val;
 
 	if(key == val) return mid;
 
@@ -20,10 +20,10 @@ int BSearch(int arr[], int key, int left, int right) {
 int main(){
 
     int L1[11] = {1,2,3,4,5,6,7,8,9,10,11};
-    int key = 2;
 
     cout << BSearch(L1, 2, 0, 10);
 
+	cout << BSearch(L1, 12, 0, 10);
 
 
     return 0;
